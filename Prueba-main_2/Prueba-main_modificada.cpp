6ed2496adce33442bc6a97725a8ae52b910111ea
Futmax1310 <94107259+Futmax1310@users.noreply.github.com>
2021-12-13 12:38:35 +0100

    Add files via upload

---
+++ Prueba-main_2/Prueba-main_modificada.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (void)
+{
+	
+string nombre;
+
+cout << "Quien eres\n";
+cin >> nombre;
+cout << "Hola " << nombre << ", que tal\n";
+
+return 0;
+}
